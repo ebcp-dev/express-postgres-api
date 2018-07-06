0f66c23f1dcc2d4486532aea9973a5f852e2e967
Earl Perez <cayetanoperez@earl-mbp.attlocal.net>
2018-07-06 11:35:10 -0700

    changes to travis config

--- test/userTest.js
+++ test/userTest.js
@@ -14,46 +14,58 @@ const userCredentials = {
     password: ''
   }
 };
-// Store authorization token
-let jwt;
-
-// before(done => {
-//   request(server)
-//     .post('/user/login')
-//     .send(userCredentials.test)
-//     .expect(200)
-//     .end((err, res) => {
-//       expect(res.body.success).to.be.true;
-//       jwt = res.body.session;
-//       done();
-//     });
-// });
 
 describe('Authentication routes', done => {
-  it('Status 200 on successful login', async () => {
+  it('Status 200 on successful signup', () => {
+    request(server)
+      .post('/user/signup')
+      .send(userCredentials.test)
+      .expect(200)
+      .end((err, res) => {
+        console.log(res.body);
+        expect(res.body.email).equal(userCredentials.test.email);
+      });
+  });
+
+  it('Status 200 on successful login', () => {
     request(server)
       .post('/user/login')
       .send(userCredentials.test)
       .expect(200)
-      .end((err, res) => {
+      .then(res => {
         expect(res.body.success).to.be.true;
-
-        it('Status 200 on authenticated route', async () => {
-          request(server)
-            .get('/user/current')
-            .set('Authorization', jwt)
-            .expect(200)
-            .end((err, res) => {
-              expect(res.body.email).equal(userCredentials.test.email);
-            });
-        });
       });
   });
 
-  it('Status 400 on wrong credentials', async () => {
+  it('Status 400 on wrong credentials', () => {
     request(server)
       .post('/user/login')
       .send(userCredentials.invalid)
       .expect(400, done);
+  });
+});
+
+describe('Protected routes', () => {
+  it('Status 200 on protected route while authenticated', () => {
+    request(server)
+      .post('/user/login')
+      .send(userCredentials.test)
+      .expect(200)
+      .then(res => {
+        request(server)
+          .get('/user/current')
+          .set('Authorization', res.body.session) // Pass in authentication from login response
+          .expect(200)
+          .end((err, res) => {
+            expect(res.body.email).equal(userCredentials.test.email);
+          });
+      })
+      .catch(err => console.log(err));
+  });
+
+  it('Status 401 on unauthorized access', done => {
+    request(server)
+      .get('/user/current')
+      .expect(401, done);
   });
 });
